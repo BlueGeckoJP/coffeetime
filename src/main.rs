ad3be823d85c8244402ae5d068b390c5ef560af4
BlueGeckoJP <kei09doya@gmail.com>
2025-11-11 09:42:48 +0900

    feat: implement the template of gtk4 application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::{Application, ApplicationWindow, glib, prelude::*};
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("me.bluegecko.coffeetime")
+        .build();
+
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Coffee Time")
+        .default_width(400)
+        .default_height(300)
+        .build();
+
+    window.present();
 }
